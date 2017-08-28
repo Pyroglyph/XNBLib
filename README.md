28caf641f26ed4e4d0c9133db095f3ef5a63c055
Pyroglyph <pyrohasmc@gmail.com>
2017-08-29 00:50:01 +0100

    Added support for a custom output folder and added an event that fires when the batch is done

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Someone please help me format this better :(
 --->
 
-# XNBLib (xnbl)
-> XNBL can be pronounced "zen-bull"
+# XNBLib
 
 XNBLib is the library behind XNBE, my XNB decompiler. It allows easy asset decompilation for all the formats that both XNA amd MonoGame support.
 
@@ -15,12 +14,34 @@ XNBLib is the library behind XNBE, my XNB decompiler. It allows easy asset decom
 1. Import the library into your project.
 2. Instantiate the Exporter class.
 3. Pass a string array of paths to XNB files into `ExportFile()`.
+4. (Optional) Also pass in a string that points to an output folder.
 4. (Optional) Subscribe to the `OnStatusUpdate` event to get console output as a string.
-5. `Run()` the Exporter and wait for the files to decompile!
+5. (Optional) Subscribe to the `OnCompleted` event to do something when XNBLib has finished.
+6. `Run()` the Exporter and wait for the files to decompile!
 
-## Basic Example
+## Examples
+### 1
 ```csharp
 var exporter = new Exporter(new [] { "C:\\path\\to\\file.xnb" });
 exporter.Run();
 ```
-After running this, your decompiled files will be placed in the same folder as their XNB counterparts.
+After this, your decompiled files will be placed in the same folder as their XNB counterparts.
+
+### 2
+```csharp
+var exporter = new Exporter(new [] { "C:\\path\\to\\file.xnb" }, "C:\\output");
+exporter.OnStatusUpdate += Exporter_OnStatusUpdate;
+exporter.Run();
+
+...
+
+void Exporter_OnStatusUpdate(string status)
+{
+    Console.WriteLine("[XNBLib] " + status);
+	// You can also leave this method blank to mute any console output.
+}
+```
+After this, your decompiled files will be placed in C:/output and [XNBLib] will be appended to the start of any console output.
+
+### Live Example
+XNBExporter is a console application I developed to showcase XNBLib. Check it out [here](https://github.com/Pyroglyph/XNBExporter)!
--- XNBLib/Exporter.cs
+++ XNBLib/Exporter.cs
@@ -9,10 +9,13 @@ namespace XNBLib
     {
         public delegate void StatusUpdated(string status);
         public event StatusUpdated OnStatusUpdate;
+        public delegate void Completed();
+        public event Completed OnCompleted;
 
         private GraphicsDeviceManager _graphics;
         private static ResourceType _type;
-        private string[] _files;
+        private readonly string[] _files;
+        private readonly string _outPath;
 
         private enum ResourceType
         {
@@ -24,27 +27,32 @@ namespace XNBLib
         /// Decompiles XNB files.
         /// </summary>
         /// <param name="files">A string array of paths to XNB files.</param>
-        public Exporter(string[] files)
+        /// <param name="outPath">The output directory where the decompiled files should go.</param>
+        public Exporter(string[] files, string outPath = "")
         {
             _graphics = new GraphicsDeviceManager(this);
             _files = files;
+            _outPath = outPath;
         }
         
         protected override void LoadContent()
         {
             for (var i = 0; i < _files.Length; i++)
-                ExportFile(_files[i]);
+                ExportFile(_files[i], _outPath);
 
-            UpdateStatus("\nAll done! Exiting...");
+            UpdateStatus("\nAll done!");
 
             // We're done!
+            OnCompleted?.Invoke();
             Exit();
-            Environment.Exit(0);
         }
 
-        private void ExportFile(string file)
+        private void ExportFile(string file, string outPath)
         {
             if (!File.Exists(file)) return;
+
+
+            if (outPath == "") outPath = Path.GetDirectoryName(file);
 
             // Validate the file before we try and parse it just in case.
             ValidateFile(file);
@@ -53,10 +61,9 @@ namespace XNBLib
             {
                 case ResourceType.Audio:
                     // Give the audio file to gameking008's SoundExporter so it can work it's wizardry.
-                    var result = SoundExporter.ConvertToWav(file);
+                    var result = SoundExporter.ConvertToWav(file, outPath);
                     if (result.Type == SoundExporter.Result.Types.Success)
                     {
-                        //Program.ClearLine();
                         Console.ForegroundColor = ConsoleColor.Green;
                         UpdateStatus("Success! Exported " + Path.GetFileNameWithoutExtension(file) + ".wav");
                         Console.ForegroundColor = ConsoleColor.Gray;
@@ -69,13 +76,12 @@ namespace XNBLib
                     }
                     break;
                 case ResourceType.Image:
-                    using (Stream stream = File.Open(file.Substring(0, file.Length - 4) + ".png", FileMode.Create))
+                    using (Stream stream = File.Open(outPath + Path.GetFileNameWithoutExtension(file) + ".png", FileMode.Create))
                     {
                         // We pass Content.Load() the texture without the .xnb extension since it is added automatically.
                         var texture = Content.Load<Texture2D>(file.Replace(".xnb", ""));
                         texture.SaveAsPng(stream, texture.Width, texture.Height);
-
-                        //Program.ClearLine();
+                        
                         Console.ForegroundColor = ConsoleColor.Green;
                         UpdateStatus("Success! Exported " + Path.GetFileNameWithoutExtension(file) + ".png");
                         Console.ForegroundColor = ConsoleColor.Gray;
@@ -99,7 +105,6 @@ namespace XNBLib
             if (s != "XNB")
             {
                 // XNB header is missing. We can't parse this file. Exit.
-                //Program.ClearLine();
                 UpdateStatus(
                     "Input Error >> File did not pass validation. This does not appear to be an XNB file.");
                 UpdateStatus("\nPress any key to exit.");
@@ -114,7 +119,6 @@ namespace XNBLib
                 // If said string contains the word sound (all XNB sounds do), then set the type accordingly.
                 _type = s.Contains("Sound") ? ResourceType.Audio : ResourceType.Image;
                 // Display the type to the user.
-                //Program.ClearLine();
                 UpdateStatus("Validated: " + Enum.GetName(typeof(ResourceType), _type));
             }
         }
